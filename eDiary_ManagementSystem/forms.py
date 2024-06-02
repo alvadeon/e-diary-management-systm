086c4c484109b81897fecf265bf27f99efc7b011
alvadeon <168619953+alvadeon@users.noreply.github.com>
2024-06-02 13:14:48 +0530

    first commit

---
+++ eDiary_ManagementSystem/forms.py
@@ -0,0 +1,7 @@
+# from django import forms
+# from .models import image
+
+# class ImageUploadForm(forms.ModelForm):
+#     class Meta:
+#         model = Image
+#         fields = ['title', 'image']
